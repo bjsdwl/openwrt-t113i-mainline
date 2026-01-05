0761424e7ca31c7e8d4c264174477840b83e0ff8
scanligithub <135568972+scanligithub@users.noreply.github.com>
2026-01-05 17:26:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
 
-# --- 1. 强力修正所有 sunxi U-Boot 配置 (扩大搜索范围) ---
-# 找到所有 U-Boot 的板级配置文件进行批量修改
+# 修正所有板型的默认频率，并对 T113 板型注入 ZQ
 find package/boot/uboot-sunxi/config/ -name "*" | xargs -n 1 sed -i 's/CONFIG_DRAM_CLK=.*/CONFIG_DRAM_CLK=792/g' 2>/dev/null
 
-# 针对 T113-S3 这个具体变体注入 ZQ 和 DDR 类型
-# OpenWrt 源码中对应的文件通常是 package/boot/uboot-sunxi/config/allwinner_t113_s3
-T113_UBOOT_CONF="package/boot/uboot-sunxi/config/allwinner_t113_s3"
-if [ ! -f $T113_UBOOT_CONF ]; then
-    # 如果找不到特定文件，就修改通用的 sunxi 配置文件
-    T113_UBOOT_CONF="package/boot/uboot-sunxi/config/sunxi"
-fi
+# 找到特定的 T113 配置文件
+T113_CONF=$(find package/boot/uboot-sunxi/config/ -name "*t113_s3*")
+if [ -z "$T113_CONF" ]; then T113_CONF="package/boot/uboot-sunxi/config/sunxi"; fi
 
-echo "Forcing T113-i parameters into $T113_UBOOT_CONF"
-sed -i '/CONFIG_DRAM_ZQ/d' $T113_UBOOT_CONF
-echo "CONFIG_DRAM_ZQ=0x7b7bfb" >> $T113_UBOOT_CONF
-sed -i '/CONFIG_DRAM_TYPE_DDR3/d' $T113_UBOOT_CONF
-echo "CONFIG_DRAM_TYPE_DDR3=y" >> $T113_UBOOT_CONF
-sed -i '/CONFIG_CONS_INDEX/c\CONFIG_CONS_INDEX=1' $T113_UBOOT_CONF
+echo "Targeting U-Boot Config: $T113_CONF"
+sed -i '/CONFIG_DRAM_ZQ/d' $T113_CONF
+echo "CONFIG_DRAM_ZQ=0x7b7bfb" >> $T113_CONF
+sed -i '/CONFIG_DRAM_TYPE_DDR3/d' $T113_CONF
+echo "CONFIG_DRAM_TYPE_DDR3=y" >> $T113_CONF
+sed -i '/CONFIG_CONS_INDEX/c\CONFIG_CONS_INDEX=1' $T113_CONF
 
-# --- 2. 镜像偏移修正 (8KB 偏移) ---
+# 修正镜像生成偏移 (BROM 魔术数查找位置)
 IMG_MAKEFILE="target/linux/sunxi/image/Makefile"
 if [ -f $IMG_MAKEFILE ]; then
     sed -i 's/CONFIG_SUNXI_UBOOT_BIN_OFFSET=128/CONFIG_SUNXI_UBOOT_BIN_OFFSET=8/g' $IMG_MAKEFILE
