58e826fb22e7e1f24f2495aa31ed6e6f374c0fd2
scanligithub <135568972+scanligithub@users.noreply.github.com>
2026-01-12 16:08:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-# 可以在此处添加额外的软件源
-# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# --- 1. 屏蔽掉会导致元数据扫描错误的源 (如 mesa/video) ---
+# 这些源在编译 U-Boot 时完全不需要，删掉它们可以极大提高构建成功率和速度
+sed -i '/video/d' feeds.conf.default
+
+# --- 2. 可选：屏蔽掉其它暂时不需要的重型源 (进一步加速) ---
+# sed -i '/luci/d' feeds.conf.default
+# sed -i '/routing/d' feeds.conf.default
+# sed -i '/telephony/d' feeds.conf.default
+
+echo "✅ Problematic feeds (video/mesa) removed from configuration."
